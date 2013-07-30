fd5f6a3ff27cbd4dab9984625fe16fbb70c5dae2
Justin Smestad <justin.smestad@gmail.com>
2013-07-30 16:53:42 -0700

    formatting

--- spec/support/matchers/default_attribute_matcher.rb
+++ spec/support/matchers/default_attribute_matcher.rb
@@ -4,6 +4,7 @@ RSpec::Matchers.define :have_a_default_of do |expected|
   match do |actual|
     actual == expected
   end
+
   failure_message_for_should do |actual|
     "expected that the default value would be #{expected}, but it was #{actual}"
   end
@@ -13,6 +14,7 @@ RSpec::Matchers.define :have_no_default do
   match do |actual|
     actual.nil?
   end
+
   failure_message_for_should do |actual|
     "expected that #{actual} would have no default and be nil"
   end
